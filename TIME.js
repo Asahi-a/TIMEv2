59018ebcfba0a8b290a4cfffc3fa790d8b078493
Asahi-Ojihineko <76908462+Asahi-Ojihineko@users.noreply.github.com>
2022-05-06 22:43:42 +0900

    Update TIME.js

--- TIME.js
+++ TIME.js
@@ -10,6 +10,7 @@ var Ts;//駅間所要時間
 var S;//勾配
 var K;//定数
 var Fr;//空走時間
+var Err = 0;//エラーの有無(0or1)
 
 Ak = prompt('加速度km/h/s');
 Ag = prompt('減速度km/h/s(正の数で)');
@@ -29,10 +30,14 @@ Vf = Vf / 3.6;
 Vh = Vh / 3.6;
 
 if (Vs > Vf && Xe < ((1.8 * ((Vs * Vs) - (Vf * Vf)))/(3.6 * Ag + S / K)) + Vh * Fr){
-  alert ('所定キョリ内で終速度まで減速できません！再読み込みして初速度をもう少し低く設定するか、次の区間と統合するなどしてください。');
+  Err = 1;
+  let hTs = document.getElementById('hTs');
+  hTs.innerHTML = '<h1>所定キョリ内で終速度まで減速できません！</h1><h2>初速度をもう少し低く設定するか、次の区間と統合するなどしてください</h2>';
 }
 
-var Xk = ((1.8 * ((Vz * Vz) - (Vh * Vh))) / (-3.6 * Ak + (S / K)));
+if (Err = 0) {
+
+var Xk = ((1.8 * ((Vs * Vs) - (Vh * Vh))) / (-3.6 * Ak + (S / K)));
 var Xg = ((1.8 * ((Vh * Vh) - (Vf * Vf))) / (3.6 * Ag + (S / K))) + (Vh * Fr);
 
 while (Xk + Xg > Xe){
@@ -46,10 +51,14 @@ while (Xk + Xg > Xe){
     if (Xk + Xg <= Xe){
       ;
     }else{
-      alert ('再読み込みして値を設定しなおすか、次の区間と統合するなどしてください。');
+      Err = 1;
+      let hTs = document.getElementById('hTs');
+      hTs.innerHTML = '<h1>所定キョリ内で終速度まで減速又は加速できません！</h1><h2>値を設定し直してください。もしくは次の区間と統合するなどしてください</h2>';
     }
   }
 }
+
+if (Err = 0) {
 
 if (Vs == Vh || Xk == 0){
   RAk = 1;
@@ -72,5 +81,8 @@ Vh = Math.round(Vh);
 
 let hTs = document.getElementById('hTs');
 hTs.innerHTML = '<h1>駅間所要時間は' + Ts + '秒です。</h1><h2>駅間最高速度は' + Vh + 'km/hです。</h2>' + Xk + '\n' + Xg + '\n' + RAg + '\n' + RAk;
+
+}
+}
 
 
