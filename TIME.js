1ef49efbf2c084309624c8ab2adc9d474476025e
Asahi-Ojihineko <76908462+Asahi-Ojihineko@users.noreply.github.com>
2022-05-06 22:46:20 +0900

    Update TIME.js

--- TIME.js
+++ TIME.js
@@ -29,7 +29,7 @@ Vs = Vs / 3.6;
 Vf = Vf / 3.6;
 Vh = Vh / 3.6;
 
-if (Vs > Vf && Xe < ((1.8 * ((Vs * Vs) - (Vf * Vf)))/(3.6 * Ag + S / K)) + Vh * Fr){
+if (Vs > Vf && Xe < ((1.8 * ((Vs * Vs) - (Vf * Vf))) / (3.6 * Ag + S / K)) + Vh * Fr) {
   Err = 1;
   let hTs = document.getElementById('hTs');
   hTs.innerHTML = '<h1>所定キョリ内で終速度まで減速できません！</h1><h2>初速度をもう少し低く設定するか、次の区間と統合するなどしてください</h2>';
@@ -37,52 +37,51 @@ if (Vs > Vf && Xe < ((1.8 * ((Vs * Vs) - (Vf * Vf)))/(3.6 * Ag + S / K)) + Vh * 
 
 if (Err = 0) {
 
-var Xk = ((1.8 * ((Vs * Vs) - (Vh * Vh))) / (-3.6 * Ak + (S / K)));
-var Xg = ((1.8 * ((Vh * Vh) - (Vf * Vf))) / (3.6 * Ag + (S / K))) + (Vh * Fr);
+  var Xk = ((1.8 * ((Vs * Vs) - (Vh * Vh))) / (-3.6 * Ak + (S / K)));
+  var Xg = ((1.8 * ((Vh * Vh) - (Vf * Vf))) / (3.6 * Ag + (S / K))) + (Vh * Fr);
 
-while (Xk + Xg > Xe){
-  Vh = Vh - 0.01;
-  if (Vh < Vs || Vh < Vf){
-    if (Vs >= Vf) {
-      Vh = Vs;
-    }else{
-      Vh = Vf;
-    }
-    if (Xk + Xg <= Xe){
-      ;
-    }else{
-      Err = 1;
-      let hTs = document.getElementById('hTs');
-      hTs.innerHTML = '<h1>所定キョリ内で終速度まで減速又は加速できません！</h1><h2>値を設定し直してください。もしくは次の区間と統合するなどしてください</h2>';
+  while (Xk + Xg > Xe) {
+    Vh = Vh - 0.01;
+    if (Vh < Vs || Vh < Vf) {
+      if (Vs >= Vf) {
+        Vh = Vs;
+      } else {
+        Vh = Vf;
+      }
+      if (Xk + Xg <= Xe) {
+        ;
+      } else {
+        Err = 1;
+        let hTs = document.getElementById('hTs');
+        hTs.innerHTML = '<h1>所定キョリ内で終速度まで減速又は加速できません！</h1><h2>値を設定し直してください。もしくは次の区間と統合するなどしてください</h2>';
+      }
     }
   }
+
+  if (Err = 0) {
+
+    if (Vs == Vh || Xk == 0) {
+      RAk = 1;
+    } else {
+      RAk = ((Vh * Vh) - (Vs * Vs)) / (2 * Xk);
+    }
+
+    if (Vf == Vh || Xg == 0) {
+      RAg = 1;
+    } else {
+      RAg = ((Vh * Vh) - (Vf * Vf)) / (2 * Xg);
+    }
+
+    Xe = ((Vh - Vs) / RAk) + ((Vh - Vs) / RAg) + ((Xe - Xk - Xg) / Vh);
+
+    //m/s→km/h、四捨五入
+    Vh = Vh * 3.6;
+    Ts = Math.round(Ts);
+    Vh = Math.round(Vh);
+
+    let hTs = document.getElementById('hTs');
+    hTs.innerHTML = '<h1>駅間所要時間は' + Ts + '秒です。</h1><h2>駅間最高速度は' + Vh + 'km/hです。</h2>' + Xk + '\n' + Xg + '\n' + RAg + '\n' + RAk;
+
+  }
 }
-
-if (Err = 0) {
-
-if (Vs == Vh || Xk == 0){
-  RAk = 1;
-}else{
-  RAk = ((Vh * Vh)-(Vs * Vs)) / (2 * Xk);
-}
-
-if (Vf == Vh || Xg == 0){
-  RAg = 1;
-}else{
-  RAg = ((Vh * Vh)-(Vf * Vf)) / (2 * Xg);
-}
-
-Xe = ((Vh - Vs) / RAk) + ((Vh - Vs) / RAg) + ((Xe - Xk - Xg) / Vh);
-
-//m/s→km/h、四捨五入
-Vh = Vh * 3.6;
-Ts = Math.round(Ts);
-Vh = Math.round(Vh);
-
-let hTs = document.getElementById('hTs');
-hTs.innerHTML = '<h1>駅間所要時間は' + Ts + '秒です。</h1><h2>駅間最高速度は' + Vh + 'km/hです。</h2>' + Xk + '\n' + Xg + '\n' + RAg + '\n' + RAk;
-
-}
-}
-
 
